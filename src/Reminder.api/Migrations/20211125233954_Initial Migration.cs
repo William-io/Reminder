6486f4710d19e771b8260fb587dbd036cfd5fb0a
William-io <capuletos@live.com>
2021-11-25 20:43:57 -0300

    Minimal completed

---
+++ src/Reminder.api/Migrations/20211125233954_Initial Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reminder.api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- src/Reminder.api/Program.cs
+++ src/Reminder.api/Program.cs
@@ -1,85 +1,112 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<ItemRepository>();
 
+var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// builder.Services.AddSingleton<ItemRepository>();
 var app = builder.Build();
 
-app.MapGet("/items", ([FromServices] ItemRepository items) =>
+app.MapGet("/items", async (ApiDbContext db) =>
 {
-    return items.Getall();
+    return await db.Items.ToListAsync();
 });
 
-app.MapPost("/items", ([FromServices] ItemRepository items, Item item) =>
+app.MapPost("/items", async (ApiDbContext db, Item item) =>
 {
-    if (items.GetById(item.id) != null)
+    if (await db.Items.FirstOrDefaultAsync(x => x.Id == item.Id) != null)
     {
-        return Results.BadRequest("Não foi possivel adicionar!!");
+        return Results.BadRequest();
     }
-    //Dando tudo certo retorna o item sob status OK!
-    items.Add(item);
-    return Results.Created($"/Items/{item.id}", item);
+
+    db.Items.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Items/{item.Id}", item);
 });
 
-app.MapGet("/items/{id}", ([FromServices] ItemRepository items, int id) =>
+app.MapGet("/items/{id}", async (ApiDbContext db, int id) =>
 {
-    var item = items.GetById(id);
+    var item = await db.Items.FirstOrDefaultAsync(x => x.Id == id);
+
     return item == null ? Results.NotFound() : Results.Ok(item);
 });
 
-app.MapPut("/items/{id}", ([FromServices] ItemRepository items, int id, Item item) =>
+app.MapPut("/items/{id}", async (ApiDbContext db, int id, Item item) =>
 {
-    if (items.GetById(id) == null)
+    var existItem = await db.Items.FirstOrDefaultAsync(x => x.Id == id);
+    if (existItem == null)
     {
-        return Results.NotFound();
+        return Results.BadRequest();
     }
-    items.Update(item);
+
+    existItem.Title = item.Title;
+    existItem.IsCompleted = item.IsCompleted;
+
+    await db.SaveChangesAsync();
     return Results.Ok(item);
 });
 
-
-app.MapDelete("/items/{id}", ([FromServices] ItemRepository items, int id) =>
+app.MapDelete("/items/{id}", async (ApiDbContext db, int id) =>
 {
-    if (items.GetById(id) == null)
+    var existItem = await db.Items.FirstOrDefaultAsync(x => x.Id == id);
+    if (existItem == null)
     {
-        return Results.NotFound();
+        return Results.BadRequest();
     }
-    items.Delete(id);
-    return Results.Ok();
+
+    db.Items.Remove(existItem);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
-app.MapGet("/", () => "Iniciando API Reminder");
+app.MapGet("/", () => "Hello from Minimal API");
 app.Run();
 
+// record Item(int id, string title, bool IsCompleted);
 
-record Item(int id, string title, bool IsCompleted);
-
-class ItemRepository
+class Item
 {
-    private Dictionary<int, Item> items = new Dictionary<int, Item>();
-    // public ItemRepository()
-    // {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
 
-    //     var firstItem = new Item(1, "First item", DateTime.Now, false);
-    //     var secondItem = new Item(Guid.NewGuid(), "Second item", DateTime.Now, false);
-    //     var thirdItem = new Item(Guid.NewGuid(), "Third item", DateTime.Now, false);
+// class ItemRepository
+// {
+//     private Dictionary<int, Item> items = new Dictionary<int, Item>();
 
-    //     items.Add(firstItem.id, firstItem);
-    //     items.Add(secondItem.id, secondItem);
-    //     items.Add(thirdItem.id, thirdItem);
-    // }
+//     public ItemRepository()
+//     {
+//         var item1 = new Item(1, "Go to the gym", false);
+//         var item2 = new Item(2, "Drink Water", true);
+//         var item3 = new Item(3, "Watch TV", false);
 
-    public IEnumerable<Item> Getall() => items.Values;
-    public Item GetById(int id)
+//         items.Add(item1.id, item1);
+//         items.Add(item2.id, item2);
+//         items.Add(item3.id, item3);
+//     }
+
+//     public IEnumerable<Item> GetAll() => items.Values;
+//     public Item GetById(int id) {
+//         if(items.ContainsKey(id))
+//         {
+//             return items[id];
+//         }
+
+//         return null;
+//     }
+//     public void Add(Item item) => items.Add(item.id, item);
+//     public void Update(Item item) => items[item.id] = item;
+//     public void Delete(int id) => items.Remove(id);
+// }
+
+class ApiDbContext : DbContext
+{
+    public DbSet<Item> Items { get; set; }
+
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
     {
-        if (items.ContainsKey(id))
-        {
-            return items[id];
-        }
-
-        return null;
     }
-    public void Add(Item item) => items.Add(item.id, item);
-    public void Update(Item item) => items[item.id] = item;
-    public void Delete(int id) => items.Remove(id);
 }
